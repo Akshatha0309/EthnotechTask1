4c0eea7db5541b09ce225b86abc1bad334ed6141
Akshatha B <akshatha0309@gmail.com>
2026-02-11 10:26:17 +0530

    class

---
+++ ClassandObject.java
@@ -0,0 +1,11 @@
+public class ClassandObject {
+    public static void main(String args[]){
+        ClassandObject CO=new ClassandObject();
+        CO.display();
+    }
+
+ void display (){
+    System.out.println("welcome to java");
+ }
+}
+
